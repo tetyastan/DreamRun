ff1f973100f76007146e227fcb4af1d3a1d5f2b7
130414 <32320593+tetyastan@users.noreply.github.com>
2026-09-09 09:34:45 +0700

    Code refactor, added Frontend debug.
    Removed monolite code, added Frontend error debug.

---
+++ frontend/src/lib/components/GameScreen.svelte
@@ -0,0 +1,35 @@
+<script>
+    import './GameScreen.css'
+
+    let {
+        currentBg,
+        nextStep,
+        currentSpeaker,
+        currentText,
+    } = $props();
+</script>
+
+<main class="container game-container">
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div 
+        class="game-screen" 
+        style="background-image: url('{currentBg || 'placeholder.jpg'}')"
+        onclick={nextStep}
+    >
+        <div class="interface-container" onclick={(e) => e.stopPropagation()}>
+        <!-- Namebox -->
+        {#if currentSpeaker}
+            <div class="name-box">
+            {currentSpeaker}
+            </div>
+        {/if}
+
+        <!-- Textbox -->
+        <div class="text-box" onclick={nextStep}>
+            <p>{currentText}</p>
+            <span class="click-hint">▼</span>
+        </div>
+        </div>
+    </div>
+</main>
---
+++ frontend/src/lib/components/MainMenu.svelte
@@ -0,0 +1,14 @@
+<script>
+    import './MainMenu.css'
+
+    let {
+        startGame,
+    } = $props();
+</script>
+
+<main class="container menu-container">
+    <div class="menu-screen">
+        <h1>DreamRun Template</h1>
+        <button onclick={startGame} class="menu-btn">Start</button>
+    </div>
+</main>
